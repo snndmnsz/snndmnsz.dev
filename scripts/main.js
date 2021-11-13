d5d955e53c94f5c9d66782b63aff53ca90e2ba6a
snndmnsz <snndmnsz98@gmail.com>
2021-11-14 00:06:22 +0300

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { magicMouse } from "./magic_mouse.js"
+import { magicMouse } from "./magic_mouse.js";
 
 let intro = document.querySelector(".intro-opening");
 let logo = document.querySelector(".logo-header");
@@ -27,17 +27,76 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  
-}else{
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+} else {
   const options = {
-    "cursorOuter": "circle-basic",
-    "hoverEffect": "pointer-overlay",
-    "hoverItemMove": false,
-    "defaultCursor": false,
-    "outerWidth": 30,
-    "outerHeight": 30
+    cursorOuter: "circle-basic",
+    hoverEffect: "pointer-overlay",
+    hoverItemMove: false,
+    defaultCursor: false,
+    outerWidth: 30,
+    outerHeight: 30,
   };
   magicMouse(options);
 }
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 35, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: false, mode: "bubble" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+requestAnimationFrame(update);
